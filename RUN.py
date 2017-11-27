377e608af1c2ca87bfebea1c6a625e42f0b627a5
shstan <shstan@gmail.com>
2017-11-27 04:07:36 -0500

    Initial commit; has login template

---
+++ RUN.py
@@ -0,0 +1,135 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import pymysql
+from tkinter import messagebox
+from tkinter import ttk
+from datetime import datetime, timedelta
+import decimal
+
+#MARTA v1.3
+#By Team 54, CS4400 2017 Fall
+class MARTA_Client:
+    def __init__(self):
+        # Invoke createLoginWindow; Invoke buildLoginWindow, Set loginWindow as mainloop
+        #Connect to the database
+        self.db = self.connect()
+        self.cursor = self.db.cursor()
+        # Login Window
+        self.createLoginWindow()
+        self.buildLoginWindow(self.loginWindow)
+        self.loginWindow.mainloop()
+        sys.exit()
+
+    ##  =======Login Window=======
+    def createLoginWindow(self):
+        # Create blank Login Window
+        self.loginWindow = Tk()
+        self.loginWindow.title("MARTA v1.3.54.1")
+
+        self.loginWindow.withdraw()
+        self.loginWindow.update_idletasks()  # Update "requested size" from geometry manager
+        x = (self.loginWindow.winfo_screenwidth() - self.loginWindow.winfo_reqwidth()) / 2
+        y = (self.loginWindow.winfo_screenheight() - self.loginWindow.winfo_reqheight()) / 2
+        self.loginWindow.geometry("+%d+%d" % (x, y))
+        self.loginWindow.deiconify()
+
+    def buildLoginWindow(self, loginWindow):
+        # Add component for Login Window
+        # Login Label
+        loginLabel = Label(loginWindow, text="Login", font="Verdana 13 bold ")
+        loginLabel.grid(row=1, column=3, sticky=W + E)
+
+        # Username Label
+        usernameLabel = Label(loginWindow, text="Username")
+        usernameLabel.grid(row=2, column=2, sticky=W)
+
+        # Password Label
+        passwordLabel = Label(loginWindow, text="Password")
+        passwordLabel.grid(row=4, column=2, sticky=W)
+
+        # # Image
+        image = Image.open("MARTA-logo.jpg")
+        image = image.resize((60, 60), Image.ANTIALIAS)
+        buzzImage = ImageTk.PhotoImage(image)
+        imageLabel = Label(loginWindow, image=buzzImage)
+        imageLabel.image = buzzImage
+        imageLabel.grid(row=2, column=4, rowspan=3, sticky=E)
+
+        # Username Entry
+        self.loginUsername = StringVar()
+        usernameEntry = Entry(loginWindow, textvariable=self.loginUsername, width=20)
+        usernameEntry.grid(row=2, column=3, sticky=W + E)
+
+        # Password Entry
+        self.loginPassword = StringVar()
+        passwordEntry = Entry(loginWindow, textvariable=self.loginPassword, show='*', width=20)
+        passwordEntry.grid(row=4, column=3, sticky=W + E)
+
+        # Login Buttons
+        loginButton = Button(loginWindow, text="Login", command=self.loginWindowLoginButtonClicked)
+        loginButton.grid(row=6, column=3)
+
+        # Register Button
+
+        registerButton = Button(loginWindow, text="Register", command=self.loginWindowRegisterButtonClicked)
+        registerButton.grid(row=6, column=4, sticky=E)
+
+    def loginWindowLoginButtonClicked(self):
+        # Click the button on Login Window:
+        # Obtain the username and password from keypress;
+        # Invoke;
+        # Invoke;
+        # Withdraw Login Window;
+        self.username = self.loginUsername.get()
+        self.password = self.loginPassword.get()
+        if not self.username:
+            messagebox.showwarning("Username input is empty", "Please enter username.")
+            return False
+        if not self.password:
+            messagebox.showwarning("Password input is empty", "Please enter password")
+            return False
+        isUsername = self.cursor.execute("SELECT * FROM User WHERE Username = %s", self.username)
+        if not isUsername:
+            messagebox.showwarning("Username is not an user\'s username",
+                                   "The username you entered is not an user\'s username.")
+            return False
+        usernameAndPasswordMatch = self.cursor.execute(
+            "SELECT * FROM User WHERE (Username = %s AND Password = %s)", (self.username, self.password))
+        if not usernameAndPasswordMatch:
+            messagebox.showwarning("Username and password don\'t match",
+                                   "Sorry, the username and password you entered"
+                                   + " do not match.")
+            return False
+
+        isManagerName = self.cursor.execute("SELECT * FROM Manager WHERE Username = %s", (self.username))
+        if isManagerName:
+            self.loginWindow.withdraw()
+            self.createChooseFunctionalityWindowManager()
+            self.buildChooseFunctionalityWindowManager(self.chooseFunctionalityWindowManager)
+        else:
+            self.loginWindow.withdraw()
+            self.createChooseFunctionalityWindow()
+            self.buildChooseFunctionalityWindow(self.chooseFunctionalityWindow)
+        return True
+
+    def loginWindowRegisterButtonClicked(self):
+        # Click button on Login Window:
+        # Invoke createNewUserRegistrationWindow; Invoke buildNewUserRegistrationWindow;
+        # Hide Login Window; Set newUserRegistrationWindow on the top
+        self.createNewUserRegistrationWindow()
+        self.buildNewUserRegistrationWindow(self.newUserRegistrationWindow)
+        self.loginWindow.withdraw()
+
+
+    # --------------------Database Connection-----------------
+    def connect(self):
+        try:
+            #TODO: figure out the name of the database
+            db = pymysql.connect(host='academic-mysql.cc.gatech.edu',
+                                 db='cs4400_Team_54', user='cs4400_Group_54', passwd='qUYP7usT')
+            return db
+        except:
+            messagebox.showwarning('Error!', 'Cannot connect. Internet Connection Issue or DB not ready.')
+            return False
+a = MARTA_Client()
+a.db.close()
